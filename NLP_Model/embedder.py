f82d4d4ce040f3961c246a158b1b894fe9d4c03e
quevenliu <108058646+quevenliu@users.noreply.github.com>
2023-08-16 17:42:01 +0800

    Constrained K means

--- NLP_Model/embedder.py
+++ NLP_Model/embedder.py
@@ -1,9 +1,4 @@
-from transformers import AutoTokenizer, AutoModel
-    
-# Get the embedding of self introduction and match message
 def get_embedding(text):
-    tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/bert-base-nli-mean-tokens")
-    model = AutoModel.from_pretrained("sentence-transformers/bert-base-nli-mean-tokens")
     input_ids = tokenizer.encode(text, return_tensors='pt')
     output = model(input_ids)
     embedding = output[0][0][0].detach().numpy()
--- NLP_Model/group.py
+++ NLP_Model/group.py
@@ -1,17 +1,23 @@
 import numpy as np
-from sklearn.cluster import KMeans
+from k_means_constrained import KMeansConstrained
 from embedder import get_embedding
+import time
 
 def group(data):
+    SIZE_MIN = 4
+    SIZE_MAX = 5
+    N_CLUSTERS=np.ceil(len(data)/SIZE_MAX).astype(int)
     user_ids = []
     embeddings = []
     for user in data:
         user_ids.append(user['user_id'])
-        embeddings.append(get_embedding(user['self_intro'] + user['match_msg']))
+        embeddings.append(get_embedding(user['self_intro'] + '\n' + user['match_msg']))
     embeddings = np.array(embeddings)
-    kmeans = KMeans(n_clusters=5, random_state=0).fit(embeddings)
+    kmeans = KMeansConstrained(n_clusters=N_CLUSTERS, 
+                               size_min=SIZE_MIN,
+                               size_max=SIZE_MAX).fit(embeddings)
     result = []
-    for i in range(5):
+    for i in range(N_CLUSTERS):
         result.append([])
     for i in range(len(user_ids)):
         result[kmeans.labels_[i]].append(user_ids[i])
