ea63988dd83b604a4e34a58b93e15ff4ea993116
Ariel Shao <syl0138@gmail.com>
2023-08-17 14:47:27 +0800

    fix: modify the layout

--- Frontend/src/components/Member.jsx
+++ Frontend/src/components/Member.jsx
@@ -3,19 +3,19 @@ import styles from "../styles/font.module.scss";
 
 export default function Member({ picture, nickname }) {
   return (
-    <div className={`${styles.content} mt-8 flex items-center `}>
+    <div className={`${styles.content} mt-5 flex items-center `}>
       {picture === "" ? (
-        <div className="w-16 h-16 bg-primaryColor rounded-full mr-6 object-cover shrink-0" />
+        <div className="w-[60px] h-[60px] bg-primaryColor rounded-full mr-5 object-cover shrink-0" />
       ) : (
         <Image
           src={picture}
           alt="Profile picture"
-          className="w-[65px] h-[65px] rounded-full mr-6 shrink-0"
-          width={65}
-          height={65}
+          className="w-[60px] h-[60px] rounded-full mr-5 shrink-0"
+          width={60}
+          height={60}
         />
       )}
-      <p className="text-[26px] font-medium">{nickname}</p>
+      <p className="text-[22px] font-semibold">{nickname}</p>
     </div>
   );
 }
